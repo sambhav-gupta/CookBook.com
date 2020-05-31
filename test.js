6e4a5dbcab965648e6498911e0343374c1df1e90
Sambhav Gupta <sambhavgupta2000@gmail.com>
2020-05-31 20:30:30 +0530

    Final Commit
    Signed-off-by: Sambhav Gupta <sambhavgupta2000@gmail.com>

--- test.js
+++
@@ -1,7 +0,0 @@
-const {db,Users,Recipes,Comments , NotificationsComments , Chats} = require('./database')
-
-
-let array = ["dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a","dssdadsad dsjdsh dsasdhb  masdjndsaklnsdkasm  aksnklsfnms lkfjaf;a"]
-
-let string = array.toString().trim()
-console.log(string.length)
