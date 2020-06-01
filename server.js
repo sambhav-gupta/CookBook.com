53287d12b082d42607d9365aede1084c3cc669e3
Sambhav Gupta <sambhavgupta2000@gmail.com>
2020-06-02 02:51:03 +0530

    database config
    Signed-off-by: Sambhav Gupta <sambhavgupta2000@gmail.com>

--- server.js
+++ server.js
@@ -12,14 +12,26 @@ const multer = require('multer')
 
 
 
-const { Pool , Client } = require('pg');
-const client = new Client({
+const { Pool } = require('pg');
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users');
+      const results = { 'results': (result) ? result.rows : null};
+      res.send(results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 // const client = new Client({
 //   connectionString: process.env.DATABASE_URL,
 //   ssl: {
@@ -141,18 +153,7 @@ app.use( session({
 app.set('view engine','hbs')
 app.set('views',__dirname + '/views')
 app.use('/',express.static(path.join(__dirname,'./public')))
-app.get('/db', async (req, res) => {
-    try {
-      const client = await client.connect();
-      const result = await client.query('SELECT * FROM users');
-      const results = { 'results': (result) ? result.rows : null};
-      res.send(results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+
 
 app.use('/signup',express.static(path.join(__dirname,'./public/signup.html')))
 
